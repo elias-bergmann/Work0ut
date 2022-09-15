e6aec4ae0fd0589f8b3ad8f20cde6cd86b3f3bc3
Elias Bergmann <elias.bergmann@heig-vd.ch>
2022-09-15 20:39:39 +0200

    Ajoutez des fichiers projet.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Workout;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnStartBtnClicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
